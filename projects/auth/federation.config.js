8db7f0a69aa4b7a2d6e6e4ec3ac055b3dbe4f5ed
Md Yasin Sumon <yasinsumon47@gmail.com>
2026-04-24 22:15:52 +0600

    feat: update federation configuration to use specific shared dependencies and improve caching in workflows

--- projects/auth/federation.config.js
+++ projects/auth/federation.config.js
@@ -1,33 +1,25 @@
-const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
+const { withNativeFederation, share } = require('@angular-architects/native-federation/config');
 
 module.exports = withNativeFederation({
   name: 'auth',
-
-
 
   exposes: {
     './Routes': './projects/auth/src/app/app.routes.ts',
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...share({
+      '@angular/core': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      '@angular/common': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      '@angular/common/http': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      '@angular/router': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      '@angular/forms': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      '@angular/animations': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      '@angular/platform-browser': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      '@angular/cdk': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      '@angular/material': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      'rxjs': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      'tslib': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    }),
   },
-
-  skip: [
-    'rxjs/ajax',
-    'rxjs/fetch',
-    'rxjs/testing',
-    'rxjs/webSocket',
-    // Add further packages you don't need at runtime
-  ],
-
-  // Please read our FAQ about sharing libs:
-  // https://shorturl.at/jmzH0
-
-  features: {
-    // New feature for more performance and avoiding
-    // issues with node libs. Comment this out to
-    // get the traditional behavior:
-    ignoreUnusedDeps: true
-  }
 });
